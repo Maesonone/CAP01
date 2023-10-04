cb11b306c249dc4831409518a1456e1a5ec085d7
unknown <ivancaroromero31@gmail.com>
2023-10-04 12:26:12 +0200

    ejercicio 2

---
+++ Ejercicio 2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Iván Caro Romero");
+        System.out.println("Calle Larios 18 4º");
+        System.out.println("999666777");
+    }
+}
