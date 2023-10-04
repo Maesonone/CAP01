0a1b3e7404a25d25165d66ed4937e88e82ff60c5
unknown <ivancaroromero31@gmail.com>
2023-10-04 12:53:10 +0200

    ejercicio 3

---
+++ Ejercicio 3/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("%-15s%s%n", "basketball", "baloncesto");
+       System.out.printf("%-15s%s%n", "rim", "aro");
+       System.out.printf("%-15s%s%n", "board", "tablero");
+       System.out.printf("%-15s%s%n", "computer", "ordenador");
+       System.out.printf("%-15s%s%n", "screen", "pantalla");
+       System.out.printf("%-15s%s%n", "head", "cabeza");
+       System.out.printf("%-15s%s%n", "mouse", "raton");
+       System.out.printf("%-15s%s%n", "keyboard", "teclado");
+       System.out.printf("%-15s%s%n", "chair", "silla");
+       System.out.printf("%-15s%s%n", "table", "mesa");
+    }
+}
