c52094731064bb233051eaa7196bbe273a481da3
unknown <ivancaroromero31@gmail.com>
2023-10-05 09:54:10 +0200

    ejercicio3

---
+++ Ejercicio 4/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.printf("%-8s%-8s%-7s%-8s%-8s%n", "Lunes", "Martes", "Mierc", "Jueves", "Viernes");
+        System.out.printf("%-8s%-8s%-7s%-8s%-8s%n", "=====", "======", "=====", "======", "=======");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "Prog.", "L.M.", "L.M.", "Prog.", "FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "Prog.", "L.M.", "L.M.", "Prog.", "FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "Prog.", "B.D.", "Prog.", "Prog", "FOL");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "S.I.", "B.D.", "Prog.", "B.D.", "S.I.");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "S.I.", "E.D.", "B.D.", "B.D.", "S.I.");
+        System.out.printf("%-8s%-8s%-8s%-8s%-8s%n", "S.I.", "E.D.", "B.D.", "E.D.", "S.I.");
+    }
+}
